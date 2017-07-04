f09f2947799106f17ae3de89e96ba5db03fd32dc
Leo de Penning <leo.depenning@illuminoo.com>
2017-07-04 13:47:51 +0200

    Annotation interface
    - Fixed mind updates
    - Improved master slave switching

--- library/src/main/java/net/ossrs/yasea/SrsEncoder.java
+++ library/src/main/java/net/ossrs/yasea/SrsEncoder.java
@@ -127,7 +127,7 @@ public class SrsEncoder {
             videoFormat.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, 0);
             videoFormat.setInteger(MediaFormat.KEY_BIT_RATE, vBitrate);
             videoFormat.setInteger(MediaFormat.KEY_FRAME_RATE, VFPS);
-            videoFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, VGOP/VFPS);
+            videoFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 0);
             vencoder.configure(videoFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
 
             // add the video tracker to muxer.

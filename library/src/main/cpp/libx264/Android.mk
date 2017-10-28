805ba647fc01df20c183927356183a9b4bcf32d2
Leo Ma <begeekmyfriend@gmail.com>
2017-10-28 10:15:33 +0800

    Add arm64-v8a
    Signed-off-by: Leo Ma <begeekmyfriend@gmail.com>

--- library/src/main/cpp/libx264/Android.mk
+++ library/src/main/cpp/libx264/Android.mk
@@ -7,6 +7,10 @@ ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
     LOCAL_SRC_FILES := libs/armeabi-v7a/libx264.a
 endif
 
+ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
+    LOCAL_SRC_FILES := libs/arm64-v8a/libx264.a
+endif
+
 ifeq ($(TARGET_ARCH_ABI),x86)
     LOCAL_SRC_FILES := libs/x86/libx264.a
 endif

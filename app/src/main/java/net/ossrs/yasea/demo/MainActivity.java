d6919266e0259f84e3f0909ecd10f5db38ac2107
obarong <dustinljr@gmail.com>
2020-04-16 10:26:45 +0800

    Add write storage permission

--- app/src/main/java/net/ossrs/yasea/demo/MainActivity.java
+++ app/src/main/java/net/ossrs/yasea/demo/MainActivity.java
@@ -73,10 +73,11 @@ public class MainActivity extends AppCompatActivity implements RtmpHandler.RtmpL
         //1. 检查是否已经有该权限
         if (Build.VERSION.SDK_INT >= 23 && (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED)) {
             //2. 权限没有开启，请求权限
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, RC_CAMERA);
+                    new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, RC_CAMERA);
         }else{
             //权限已经开启，做相应事情
             isPermissionGranted = true;
